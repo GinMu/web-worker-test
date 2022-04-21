f6478724fc57d18dcb99e3467390ff02a939d813
GinMu <gin.musheng@gmail.com>
2022-04-21 12:34:01 +0800

    test web3

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,27 @@
 </template>
 
 <script>
+import { NFTDao } from "@jccdex/ethereum-contract";
+import Web3 from "web3";
 
 export default {
-  name: 'App',
-  created () {
+  name: "App",
+  created() {
     try {
-      const wallet = require("@swtc/wallet").Factory({guomi: true})
-      console.log(wallet.generate())
-
-      console.log(wallet.isValidSecret("snWmmCRCnAckD9D5sJApvBnpPskh3"))
-      console.log(wallet.isValidAddress("jwsn4igtyMik5Me78tJ67LXkPAgd5eifxU"))
+      const options = {
+        account: "0xb5e35844189B30B766f6CB0A2Fa5b146225B9B1D",
+        chainId: 256,
+        web3: new Web3(new Web3.providers.HttpProvider("http://localhost:8545")),
+        nftDaoContract: "0xd5F63547b8DF32C33e9be230911b9Ada78D33616",
+        multicallAddress: "0x2aB24613Ddc1F33C82E97a86C1D48bF515cCB650"
+      };
+      const nftDao = new NFTDao(options);
+      console.log(nftDao)
     } catch (error) {
       console.log(error);
     }
   }
-}
+};
 </script>
 
 <style>
