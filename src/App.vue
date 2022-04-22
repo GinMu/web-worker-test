369e2211cc2bfc8bdcc8948352df96c5085b2d21
GinMu <gin.musheng@gmail.com>
2022-04-22 10:09:19 +0800

    test nft

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,31 @@
 </template>
 
 <script>
-import { NFTDao } from "@jccdex/ethereum-contract";
-import Web3 from "web3";
+// import { NFTDao } from "@jccdex/ethereum-contract";
+// import Web3 from "web3";
+import  NFTTransaction from "@jccdex/nft-cross-chain-protocol";
 
 export default {
   name: "App",
+  mounted() {
+    console.log(NFTTransaction)
+  },
   created() {
     try {
-      const options = {
-        account: "0xb5e35844189B30B766f6CB0A2Fa5b146225B9B1D",
-        chainId: 256,
-        web3: new Web3(new Web3.providers.HttpProvider("http://localhost:8545")),
-        nftDaoContract: "0xd5F63547b8DF32C33e9be230911b9Ada78D33616",
-        multicallAddress: "0x2aB24613Ddc1F33C82E97a86C1D48bF515cCB650"
-      };
-      const nftDao = new NFTDao(options);
-      console.log(nftDao)
+      // const options = {
+      //   account: "0xb5e35844189B30B766f6CB0A2Fa5b146225B9B1D",
+      //   chainId: 256,
+      //   web3: new Web3(new Web3.providers.HttpProvider("http://localhost:8545")),
+      //   nftDaoContract: "0xd5F63547b8DF32C33e9be230911b9Ada78D33616",
+      //   multicallAddress: "0x2aB24613Ddc1F33C82E97a86C1D48bF515cCB650"
+      // };
+      // const nftDao = new NFTDao(options);
+      // console.log(nftDao)
+
+
+      const nftTransaction = new NFTTransaction("jsAhWLCakwvxxKw1Fn2KewnQyKxmZDT6Jj", [""]);
+
+      console.log(nftTransaction)
     } catch (error) {
       console.log(error);
     }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Worker from "worker-loader!./period-fetch-balance.worker";
+// import Worker from "worker-loader!./period-fetch-balance.worker";
 
 createApp(App).mount('#app')
 
-let time = 1;
+// let time = 1;
 
-const worker = new Worker();
+// const worker = new Worker();
 
-worker.postMessage({
-    method: "fetchBalance",
-    address: "jsk45ksJZUB7durZrLt5e86Eu2gtiXNRN4"
-});
+// worker.postMessage({
+//     method: "fetchBalance",
+//     address: "jsk45ksJZUB7durZrLt5e86Eu2gtiXNRN4"
+// });
 
-worker.onmessage= (e) => {
-    console.log("fetch time ", time, ": ", e.data);
-    time++;
-}
+// worker.onmessage= (e) => {
+//     console.log("fetch time ", time, ": ", e.data);
+//     time++;
+// }
 
 
-setTimeout(() => {
-    worker.postMessage({
-        method: "stopFetch"
-    })
-}, 60000);
+// setTimeout(() => {
+//     worker.postMessage({
+//         method: "stopFetch"
+//     })
+// }, 60000);
